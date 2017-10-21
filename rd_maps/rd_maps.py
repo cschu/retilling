14cf35e6b9ad185d8bb6a7bee7ea7b90c74cb787
Christian Schudoma <cschu1981@gmail.com>
2017-10-22 00:32:20 +0100

    Porting complete.

--- rd_maps/rd_maps.py
+++ rd_maps/rd_maps.py
@@ -35,7 +35,15 @@ VCFFILTER = 'gzip -dc {0} | grep \'^#\' | gzip > {1}; gzip -dc {0} | grep -v \'#
 
 HHFILTER = 'gzip -dc {} | awk -v OFS=\'\\t\' \'{{ split(\$10, data, \\":\\"); split(data[2], ad, \\",\\"); if (data[1] == \\"0/1\\" && (ad[1] > 0 || ad[2] > 0)) {{ if ( ad[2] / (ad[1] + ad[2]) > 0.1999 && ad[2] >= 5) print \$0; }} else if (data[1] == \\"1/1\\" && ad[2] >= 3) print \$0; }}\' | gzip > {}; touch {};'
 
-DIFILTER = 'gzip -dc {} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c[\$1\$3]++; next; }}; c[\$1\$2] > 0\' {} - | awk -v OFS=\'\\t\' \'/^#/{{print \$0; next;}} {{ split(\$10, data, \\":\\"); split(data[2], ad, \\",\\"); if (data[1] == \\"0/1\\" && (ad[1] > 0 || ad[2] > 0)) {{ if ( ad[2] / (ad[1] + ad[2]) > 0.1999 && ad[2] >= 5) print \$0; }} else if (data[1] == \\"1/1\\" && ad[2] >= 3) print \$0; }}\' > {}; touch {};'
+DIFILTER = 'source vcftools-0.1.13; gzip -dc {0} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c[\$1\$3]++; next; }}; c[\$1\$2] > 0\' {1} - | awk -v OFS=\'\\t\' \'/^#/{{print \$0; next;}} {{ split(\$10, data, \\":\\"); split(data[2], ad, \\",\\"); if (data[1] == \\"0/1\\" && (ad[1] > 0 || ad[2] > 0)) {{ if ( ad[2] / (ad[1] + ad[2]) > 0.1999 && ad[2] >= 5) print \$0; }} else if (data[1] == \\"1/1\\" && ad[2] >= 3) print \$0; }}\' | bgzip -c > {2} && tabix -p vcf {2}; touch {3};'
+
+INTERSECT = 'source vcftools-0.1.13; vcf-isec -f -c {} {} > {}; touch {};'
+
+#vcfout=$(dirname $vcfin)/$(basename $vcfin .gz).nod.gz
+#gzip -dc $vcfin | grep -v '##DRAGEN' | bgzip -c > $vcfout && tabix -p vcf $vcfout;
+
+#system("vcf-isec -f -c $data2[$string0] $data[$string1] $data[$string2] $data[$string3] $data[$string4] $data[$string5] $data[$string6] $data[$string7] $data[$string8] $data[$string9] $data[$string10] $data[$string11] $data[$string12] $data[$string13] $data[$string14] $data[$string14] $data[$string15] $data[$string16] $data[$string17] $data[$string18] $data[$string19] $data[$string20] $data[$string21] $data[$string22] $data[$string23] $data[$string24] $data[$string25] $data[$string26] $data[$string27] /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1116_LIB10020_LDI8275.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1128_LIB10021_LDI8276.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1128_LIB10022_LDI8277.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1132_LIB10313_LDI8531.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1132_LIB10314_LDI8532.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1132_LIB10315_LDI8533.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1132_LIB10316_LDI8534.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1133_LIB10317_LDI8535.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1133_LIB10318_LDI8536.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1134_LIB10495_LDI8690.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1147_LIB10700_LDI8902.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1147_LIB10701_LDI8903.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1149_LIB10703_LDI8905.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1158_LIB11007_LDI9084.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1162_LIB11008_LDI9085.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1162_LIB11009_LDI9086.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1170_LIB11492_LDI9431.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1170_LIB11493_LDI9432.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1171_LIB11494_LDI9433.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1171_LIB11495_LDI9434.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1176_LIB11490_LDI9429.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1176_LIB11491_LDI9430.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1238_LIB10702_LDI8904.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1238_LIB10704_LDI8906.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1238_LIB10705_LDI8907.vcf.2.gz /home/data/wheat_ems_mutation/Dragen_cadenza_controls/Sample_1238_LIB10706_LDI8908.vcf.2.gz > $data[$string0].filtered.unique.vcf\n") == 0  or die "system vcf isec filter job failed: $?"; 
+
 
 """
 
@@ -105,6 +113,9 @@ if __name__ == '__main__':
     VCFFILTER_DONE = os.path.join(workdir, 'vcffilter.done')
     HHFILTER_DONE = os.path.join(workdir, 'hhfilter.done')
     DIFILTER_DONE = os.path.join(workdir, 'difilter.done')
+    INTERSECT_DONE = os.path.join(workdir, 'intersect.done')
+    ALL_DONE = os.path.join(workdir, 'all.done')
+    FINAL_OUTPUT = 'unique_snps.vcf'
 
     with open(sys.argv[1]) as vcf_in:
         directories = list(line.strip() for line in vcf_in)
@@ -153,6 +164,7 @@ VCFFILTER = 'gzip -dc {0} | grep \'^#\' | gzip > {1}; gzip -dc {0} | grep -v \'#
 
 DIFILTER = 'gzip -dc {} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c[\$1\$3]++; next; }}; c[\$1\$2] > 0\' {} - | awk -v OFS=\'\\t\' \'/^#/{{print \$0; next;}} {{ split(\$10, data, \\":\\"); split(data[2], ad, \\",\\"); if (data[1] == \\"0/1\\" && (ad[1] > 0 || ad[2] > 0)) {{ if ( ad[2] / (ad[1] + ad[2]) > 0.1999 && ad[2] >= 5) print \$0; }} else if (data[1] == \\"1/1\\" && ad[2] >= 3) print \$0; }}\' > {}; touch {};'
 
+DIFILTER = 'source vcftools-0.1.13; gzip -dc {0} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c[\$1\$3]++; next; }}; c[\$1\$2] > 0\' {1} - | awk -v OFS=\'\\t\' \'/^#/{{print \$0; next;}} {{ split(\$10, data, \\":\\"); split(data[2], ad, \\",\\"); if (data[1] == \\"0/1\\" && (ad[1] > 0 || ad[2] > 0)) {{ if ( ad[2] / (ad[1] + ad[2]) > 0.1999 && ad[2] >= 5) print \$0; }} else if (data[1] == \\"1/1\\" && ad[2] >= 3) print \$0; }}\' | bgzip -c > {2} && tabix -p vcf {2}; touch {3};'
 
     """
     if not os.path.exists(DIFILTER_DONE):
@@ -160,7 +172,7 @@ DIFILTER = 'gzip -dc {} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c
         for vf in vcffiles:
             prefix = os.path.join(workdir, os.path.basename(vf))
             donefiles.append(prefix + '.done')
-            fvcffiles.append(prefix.replace('.vcf.nod.gz', '.filtered.vcf'))
+            fvcffiles.append(prefix.replace('.vcf.nod.gz', '.filtered.vcf.gz'))
             cmd = DIFILTER.format(vf, MULTICOV_OUTPUT, fvcffiles[-1], donefiles[-1])
             sbatch = SBATCH.format('ei-medium', '8GB', 1, cmd)
             pr = sub.Popen(sbatch, stdout=sub.PIPE, stderr=sub.PIPE, stdin=sub.PIPE, shell=True)
@@ -178,9 +190,55 @@ DIFILTER = 'gzip -dc {} | awk -F\'\\t\' \'/^#/{{ print \$0; next; }}; NR==FNR{{c
         out, err = pr.communicate()
 
     else:
-        fvcffiles = list(os.path.join(workdir, os.path.basename(vf)).replace('.vcf.nod.gz', '.filtered.vcf') for vf in vcffiles) 
+        fvcffiles = list(os.path.join(workdir, os.path.basename(vf)).replace('.vcf.nod.gz', '.filtered.vcf.gz') for vf in vcffiles) 
         print('FVCFFILES', fvcffiles)
 
+    """
+    INTERSECT = 'source vcftools-0.1.13; vcf-isec -f -c {} {} > {}; touch {};'
+    """
+    if not os.path.exists(INTERSECT_DONE):
+        donefiles, uniqfiles = list(), list()
+        isect_sets = set(fvcffiles)
+        for vf in fvcffiles:
+            donefiles.append(vf + '.done')
+            uniqfiles.append(vf.replace('.filtered.vcf.gz', '.uniq.vcf'))
+            cmd = INTERSECT.format(vf, ' '.join(isect_sets.difference([vf])), uniqfiles[-1], donefiles[-1])
+            sbatch = SBATCH.format('ei-medium', '8GB', 1, cmd)
+            pr = sub.Popen(sbatch, stdout=sub.PIPE, stderr=sub.PIPE, stdin=sub.PIPE, shell=True)
+            out, err = pr.communicate()
+        tstart = time.time()
+
+        while True:
+            donefiles = list(f for f in donefiles if not os.path.exists(f))
+            if not donefiles or time.time() - tstart > 24 * 3600:
+                break
+            time.sleep(300)
+
+        pr = sub.Popen('touch {};'.format(INTERSECT_DONE), shell=True, stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
+        out, err = pr.communicate()
+    else:
+        uniqfiles = list(vf.replace('.filtered.vcf.gz', '.uniq.vcf') for vf in fvcffiles)
+
+    vcfheader = []
+    snps = list()
+    for uf in uniqfiles: 
+        sampleID = os.path.basename(uf).split('.')[0]
+        with open(uf) as _in:
+            for row in csv.reader(_in, delimiter='\t'):
+                if not row[0].startswith('#'):
+                    row[1], row[2] = int(row[1]), sampleID
+                    snps.append(row)
+    with open(FINAL_OUTPUT, 'w') as final_out:
+        for line in open(uniqfiles[0]):
+            if line.startswith('#') and not line.startswith('##reference') and not line.startswith('##source'):
+                print(line.strip(), file=final_out)
+            else:
+                break
+        for snp in sorted(snps, key=lambda x:(x[0], x[1])):
+            print(*snp, sep='\t', file=final_out)
+
+    pr = sub.Popen('touch {};'.format(ALL_DONE), shell=True, stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
+    out, err = pr.communicate() 
 
     sys.exit(1)
 
